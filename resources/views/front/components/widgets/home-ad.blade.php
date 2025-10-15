d8068108c248a05b7944c8c518d08121273449dd
kifa7mady <34715387+kifa7mady@users.noreply.github.com>
2025-10-15 23:49:56 +0300

    push
    push

--- resources/views/front/components/widgets/home-ad.blade.php
+++ resources/views/front/components/widgets/home-ad.blade.php
@@ -2,5 +2,5 @@
     <?php include('front/widgets/home-ad/home-ad.css')?>
 </style>
 <div class="home-ad-widget">
-    <img src="{!! asset('front/widgets/home-ad/ad-2.png') !!}" />
+    <img src="{!! asset('front/widgets/home-ad/ad-3.png') !!}" />
 </div>
