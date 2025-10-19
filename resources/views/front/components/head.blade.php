e98425cc091395ad75994351727beaea2b51094d
kifa7mady <34715387+kifa7mady@users.noreply.github.com>
2025-10-19 18:00:42 +0300

    push
    push

--- resources/views/front/components/head.blade.php
+++ resources/views/front/components/head.blade.php
@@ -29,6 +29,7 @@
 <meta property="og:type" content="website">
 
 <meta name="apple-mobile-web-app-capable" content="yes">
+<meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 <meta name="theme-color" content="#013047">
 
 
