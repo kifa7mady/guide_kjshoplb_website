739d3024d00c402d166a70ae610d2ec7124ad026
kifa7mady <34715387+kifa7mady@users.noreply.github.com>
2025-11-23 23:33:02 +0200

    adding api
    adding api

---
+++ app/Http/Controllers/Api/GuideHomeController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Models\Region;
+use App\Models\Category;
+use Illuminate\Http\JsonResponse;
+
+class GuideHomeController extends Controller
+{
+    public function home(): JsonResponse
+    {
+        $categories = Category::select('categories.id', 'categories.name', 'categories.logo')
+            ->where('parent_id', '>', 0)
+            ->with([
+                'parent',
+                'CustomerJobsByCategory',
+            ])
+            ->withCount('CustomerJobsByCategory')
+            ->has('CustomerJobsByCategory', '>', 1)
+            ->orderByDesc('customer_jobs_by_category_count')
+            ->orderBy('priority')
+            ->limit(2)
+            ->get();
+
+        return response()->json([
+            'status' => true,
+            'data' => $categories,
+        ]);
+    }
+}
